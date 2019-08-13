56c83f7f666097d0f0ba8861bc9ee70b276b3181
Aleksa Kovacevic <48535139+TheLexa@users.noreply.github.com>
2019-08-13 15:09:33 +0200

    Bug fix v1.1

--- Calculator/main.js
+++ Calculator/main.js
@@ -104,111 +104,131 @@ function runBack() {
     var val = document.case.display.value.slice(0, -1);
     document.case.display.value = val;
 
-    // pop last element from the array
+if (queue.length > 1){
+        // pop last element from the array
     let last = queue.pop();
     // check if element length is more than 1
     if (last.length > 1) {
         // remove last character from string and push to the array again
         queue.push(last.slice(0, -1))
     }
-
+}
 };
+
 function testLength() {
 
-    if (document.case.display.value.length  >16 &&  document.case.display.value.length < 21) {
+    if (document.case.display.value.length > 16 && document.case.display.value.length < 21) {
 
-      Notiflix.Notify.Info('Because you have a lot of charatchers font size is smaller');
-      document.getElementById("display").style.fontWeight = "550";
-      document.getElementById("display").style.fontSize = "2em";
-} else if (document.case.display.value.length > 20) {
-    var str = document.case.display.value.length
-    Notiflix.Notify.Warning('Max characters you can see is 28 ');
-    Notiflix.Notify.Failure('Number of your characters' + str);
+        Notiflix.Notify.Info('Because you have a lot of charatchers font size is smaller');
+        document.getElementById("display").style.fontWeight = "550";
+        document.getElementById("display").style.fontSize = "2em";
+    } else if (document.case.display.value.length > 20) {
+        var str = document.case.display.value.length
+        Notiflix.Notify.Warning('Max characters you can see is 28 ');
+        Notiflix.Notify.Failure('Number of your characters' + str);
 
-    document.getElementById("display").style.fontWeight = "500";
-    document.getElementById("display").style.fontSize = "1.5em";
+        document.getElementById("display").style.fontWeight = "500";
+        document.getElementById("display").style.fontSize = "1.5em";
+    } else {
+        document.getElementById("display").style.fontWeight = "500";
+        document.getElementById("display").style.fontSize = "2.5em";
+    }
 }
-else {
-    document.getElementById("display").style.fontWeight = "500";
-    document.getElementById("display").style.fontSize = "2.5em";
-}}
 
-    document.addEventListener("DOMContentLoaded", function(event) {
+document.addEventListener("DOMContentLoaded", function(event) {
     var numbers = document.querySelectorAll(".digit, #back")
     numbers.forEach(el => el.addEventListener('click', testLength))
 });
 
 function runEquals() {
-    if ( document.case.display.value.length < 3 ) {
+    if (document.case.display.value.length < 3) {
         Notiflix.Notify.Info('Enter charatchers !');
-    }else if (isNaN(document.case.display.value) ) {
-        var equals = Math.round(eval(document.case.display.value)* 1000) / 1000;
+    } else if (isNaN(document.case.display.value)) {
+        var equals = Math.round(eval(document.case.display.value) * 1000) / 1000;
         document.case.display.value = equals;
         document.getElementById("result").innerHTML += queue.join("") + "=" + equals + "\n";
         queue = [equals.toString()];
         document.getElementById('back').value = "CE";
-        document.getElementById('back').onclick = runBack ;
-    }else if (document.case.display.value == "Infinity" ) {
-       document.getElementById('back').value = "AC";
-        document.getElementById('back').onclick = DeleteAll ;
+        document.getElementById('back').onclick = runBack;
+        Notiflix.Notify.Success(' ');
+    } else if (document.case.display.value == "Infinity") {
+        document.getElementById('back').value = "AC";
+        document.getElementById('back').onclick = DeleteAll;
         Notiflix.Notify.Warning(' Infinity ! ');
 
     } else {
         document.getElementById('back').value = "CE";
-        document.getElementById('back').onclick = runBack ;
+        document.getElementById('back').onclick = runBack;
         Notiflix.Notify.Warning(' Can not be calculated ! ');
 
     }
 }
 
-function testNum(){
-    if (document.case.display.value == "Infinity"  ) {
-      document.getElementById('back').value = "AC";
-       document.getElementById('back').onclick = DeleteAll ;
-       Notiflix.Notify.Warning(' Infinity ! ');
-    }else if (document.case.display.value == "NaN") {
+function testNum() {
+    if (document.case.display.value == "Infinity") {
         document.getElementById('back').value = "AC";
-         document.getElementById('back').onclick = DeleteAll ;
-         Notiflix.Notify.Warning(' Not a Number ! ');
-     }else if (document.case.display.value == "0/0" ) {
-             Notiflix.Notify.Warning(' You cannot divide by 0 ! ');
-   } else {
-       document.getElementById('back').value = "CE";
-       document.getElementById('back').onclick = runBack ;
-}}
+        document.getElementById('back').onclick = DeleteAll;
+        Notiflix.Notify.Warning(' Infinity ! ');
+    } else if (document.case.display.value == "NaN") {
+        document.getElementById('back').value = "AC";
+        document.getElementById('back').onclick = DeleteAll;
+        Notiflix.Notify.Warning(' Not a Number ! ');
+    } else if (!document.case.display.value.includes("")) {} else if (document.case.display.value.includes("/0")) {
+        Notiflix.Notify.Failure(' You cannot divide by 0 ! ');
+    } else if (document.case.display.value.includes("..") || document.case.display.value.includes("//") || document.case.display.value.includes("**") || document.case.display.value.includes("--") || document.case.display.value.includes("++")) {
+        runBack();
+        Notiflix.Notify.Failure('Enter number ! ');
+    } else if (!document.case.display.value.includes("(") && document.case.display.value.includes(")")) {
+        Notiflix.Notify.Failure('U cannot end bracket now !');
+    } else if (document.case.display.value.includes(")") && !/([123456789])/.test(document.case.display.value)) {
+        Notiflix.Notify.Failure('U cannot end bracket now !');
+    } else if (document.case.display.value.includes(")") && !/([+/*-])/.test(document.case.display.value)) {
+        Notiflix.Notify.Failure('U cannot end bracket now !');
+    } else if (!document.case.display.value.includes("(") && document.case.display.value.includes(")") && !/([+/*-])/.test(document.case.display.value)) {
+        Notiflix.Notify.Failure('U cannot end bracket now !');
+    } else if (!document.case.display.value.includes("(") && document.case.display.value.includes(")") && !/([+/*-])/.test(document.case.display.value) && !/([123456789])/.test(document.case.display.value)) {}
+        else {
+        document.getElementById('back').value = "CE";
+        document.getElementById('back').onclick = runBack;
+    }
+}
+
 document.addEventListener("DOMContentLoaded", function(event) {
-var numbers = document.querySelectorAll(".digit, .oper")
-numbers.forEach(el => el.addEventListener('click', testNum))
+    var numbers = document.querySelectorAll(".digit, .oper")
+    numbers.forEach(el => el.addEventListener('click', testNum))
 });
 Notiflix.Confirm.Init({
-        timeout: 3000,
-	    okButtonBackground:"#C46600",
-		titleColor:"#C46600",
-    });
-    function DeleteAll() {
-        document.case.display.value = "";
-    }
-    function Del() {
-     Notiflix.Confirm.Show( ' Confirm',
-     'Are you sure you want to delete text?', 'Yes', 'No', function(){
-         Notiflix.Notify.Success('Text is Deleted');
-         document.getElementById("result").innerHTML = "";
- },
-  function(){
-      Notiflix.Notify.Info('Text is not Deleted');
-     } );
+    timeout: 3000,
+    okButtonBackground: "#C46600",
+    titleColor: "#C46600",
+});
+
+function DeleteAll() {
+    document.case.display.value = "";
+}
+
+function Del() {
+    Notiflix.Confirm.Show(' Confirm',
+        'Are you sure you want to delete text?', 'Yes', 'No',
+        function() {
+            Notiflix.Notify.Success('Text is Deleted');
+            document.getElementById("result").innerHTML = "";
+        },
+        function() {
+            Notiflix.Notify.Info('Text is not Deleted');
+        });
 }
 
 //print
 function printTextArea() {
-  childWindow = window.open('','childWindow','location=yes, menubar=yes, toolbar=yes');
-  childWindow.document.open();
-  childWindow.document.write('<html><head></head><body>');
-  childWindow.document.write(document.getElementById('result').value.replace(/\n/gi,'<br>'));
-  childWindow.document.write('</body></html>');
-  childWindow.print();
-  childWindow.document.close();
-  childWindow.close();
+    childWindow = window.open('', 'childWindow', 'location=yes, menubar=yes, toolbar=yes');
+    childWindow.document.open();
+    childWindow.document.write('<html><head></head><body>');
+    childWindow.document.write(document.getElementById('result').value.replace(/\n/gi, '<br>'));
+    childWindow.document.write('</body></html>');
+    childWindow.print();
+    childWindow.document.close();
+    childWindow.close();
 }
 //Font ++ and --
 function FontM() {
@@ -232,45 +252,39 @@ function FontP() {
         txt.style.fontSize = (currentSize + 5) + 'px';
     }
 }
-window.onload=function(){
-//local storage savebtn
-var note_textarea = document.querySelector('#TE');
-var result_textarea = document.querySelector('#result');
-var save_button = document.querySelector('#SaveBtn');
-var display = document.querySelector('#display');
-
-save_button.addEventListener('click', updateOutput);
-
-result_textarea.textContent = localStorage.getItem('content1');
-note_textarea.textContent = localStorage.getItem('content');
-display.value = localStorage.getItem('content_display');
-
-function updateOutput() {
-    Notiflix.Notify.Success('Text  has been saved ');
-	localStorage.setItem('content', note_textarea.value);
-    localStorage.setItem('content1', result_textarea.value);
-    localStorage.setItem('content_display',display.value);
-
-}
-
 window.onload = function() {
- const myInput = document.getElementById('display');
- myInput.onpaste = function(e) {
-   e.preventDefault();
- }
-}
+    //local storage savebtn
+    var note_textarea = document.querySelector('#TE');
+    var result_textarea = document.querySelector('#result');
+    var save_button = document.querySelector('#SaveBtn');
+    var display = document.querySelector('#display');
+
+    save_button.addEventListener('click', updateOutput);
+
+    result_textarea.textContent = localStorage.getItem('content1');
+    note_textarea.textContent = localStorage.getItem('content');
+    display.value = localStorage.getItem('content_display');
+
+    function updateOutput() {
+        Notiflix.Notify.Success('Text  has been saved ');
+        localStorage.setItem('content', note_textarea.value);
+        localStorage.setItem('content1', result_textarea.value);
+        localStorage.setItem('content_display', display.value);
+
+    }
+
+    window.onload = function() {
+        const myInput = document.getElementById('display');
+        myInput.onpaste = function(e) {
+            e.preventDefault();
+        }
+    }
 
 }
+
 function Git() {
     window.open("https://github.com/TheLexa", "_blank");
 };
-//lock scrolling
-/*
-$('body').css({'overflow':'hidden'});
- $(document).bind('scroll',function () {
-      window.scrollTo(0,0);
- });
-*/
 
 // NUMBERS
 /*
@@ -287,16 +301,5 @@ wage.addEventListener("keydown", function (e) {
     if (e.keyCode === 13) {  //checks whether the pressed key is "Enter"
          runEquals();
     }
-});
-*/
-
-
-/*
-$(document).ready(function() {
-  // ••• Show alert in a pop-up message box
-  //alert("Hello Wordl!")
-  $('button#comma').click(function() {
-    alert('Hello World!');
-  });
 });
 */
